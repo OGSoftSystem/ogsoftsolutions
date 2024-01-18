c5428abefbc3e7682f5ef5cc9ae7464ba6604648
chukumela <chukumelawilson@gmail.com>
2024-01-18 23:07:25 +0100

    Fix: replaced deprecated dependency.

--- components/atom/DynamicText.tsx
+++ components/atom/DynamicText.tsx
@@ -1,28 +1,37 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import ReactTyped from "react-typed";
+import { useEffect, useState } from "react";
+import TextTransition, { presets } from "react-text-transition";
 
+const TEXTS = [
+  "Hospital Management",
+  "Unified Payment",
+  "Secure Services",
+  "Quality output",
+];
 
 const DynamicText = () => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      2000 // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+
   return (
     <div className="w-full md:-mt-20">
-      <h1 className={cn("text-[1.70rem] sm:text-4xl md:text-5xl lg:leading-[4.5rem] poppins-heading font-bold gradient-text leading-[2.5rem]")}>
-
+      <h1 className="text-[1.70rem] sm:text-4xl md:text-5xl lg:leading-[4.5rem] poppins-heading leading-[2.5rem] gradient-text">
         Meet Africa&apos;s finest in <br />
-        <ReactTyped
-          strings={[
-            "Hospital Management",
-            "Unified Payment",
-            "Secure Services",
-            "Quality output",
-          ]}
-          typeSpeed={50}
-          loop
-          backSpeed={15}
-          cursorChar="✍️"
-          showCursor={true}
-        />
+        <TextTransition
+          springConfig={presets.wobbly}
+          className={cn("text-blue-800")}
+        >
+          {TEXTS[index % TEXTS.length]}
+        </TextTransition>
       </h1>
     </div>
   );
