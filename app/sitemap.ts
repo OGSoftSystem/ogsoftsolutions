7bcb5a5843198898134e151d9d47f8cb2dd7e538
chukumela <chukumelawilson@gmail.com>
2024-10-28 21:23:35 +0100

    fix: add url to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,6 +23,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       url: `${siteConfig.baseUrl}/faq`,
     },
     {
+      url: `${siteConfig.baseUrl}/contact-us`,
+    },
+    {
       url: `${siteConfig.baseUrl}/pricing`,
     },
     ...postEntries,
